b05a14ad4d14baf8d982c8d183466dd033d52dd4
lewyczlowiek <tomek.leewandowski@gmail.com>
2025-03-05 16:16:56 +0100

    Inital commit

---
+++ src/main/java/com/example/ProgressoApp/ProgressoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgressoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressoAppApplication.class, args);
+	}
+
+}
